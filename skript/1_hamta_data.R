069c6fb0b245217c1a31de25768fbfb263f7e561
Mats Andersson <mats.b.andersson@regiondalarna.se>
2025-02-27 16:23:22 +0100

    Kompletteringar i markdownfil samt hämta data.

--- skript/1_hamta_data.R
+++ skript/1_hamta_data.R
@@ -9,4 +9,13 @@ hoppa_over_felhantering = TRUE
 
 source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R", encoding = "utf-8", echo = FALSE)
 
-
+# Utbildningsnivå från 85 och framåt uppdelat på kön. Data hämtas i detta fall från GGplot-objektet (när data används i markdown) FEL
+source("https://raw.githubusercontent.com/Region-Dalarna/diagram/main/diag_utbniva_flera_diagram_scb.R")
+#gg_utbniva_85 <- diag_utbniva_tidserie_och_lansjmfr(region_vekt = c("20"),
+                                                    output_mapp = Output_mapp_figur,
+                                                    diagram_capt = "Källa: SCB:s öppna statistikdatabas.\nBearbetning: Samhällsanalys, Region Dalarna",
+                                                    skapa_fil = spara_diagram_som_bildfiler,
+                                                    diag_hogutb_over_tid = TRUE,
+                                                    diag_lagutb_over_tid = TRUE,
+                                                    diag_andel_alla_utbnivaer = TRUE,
+                                                    vald_utb_niva = "hogutb")
