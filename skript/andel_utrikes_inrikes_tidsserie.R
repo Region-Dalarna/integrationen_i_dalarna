b2ecb8fadd1c583410ff19c85b06c75b54e27663
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-09-22 15:01:46 +0200

    Ändrat i diagramskript

--- skript/andel_utrikes_inrikes_tidsserie.R
+++ skript/andel_utrikes_inrikes_tidsserie.R
@@ -1,4 +1,6 @@
 diag_bef_inr_utr_tid <- function(region = "20", # Enbart ett län i taget.
+                                 diag_andel = TRUE, # Andel inrikes/utrikes födda i arbetsför ålder
+                                 diag_antal = FALSE, # Antal "-"
                                  visa_logga_i_diagram = TRUE,                        # TRUE om logga ska visas i diagrammet, FALSE om logga inte ska visas i diagrammet
                                  logga_sokvag = NA,                                 # sökväg till logga som ska visas i diagrammet
                                  output_mapp = "G:/Samhällsanalys/API/Fran_R/utskrift/",                                  # mapp där diagram ska sparas, NA = sparas ingen fil
@@ -30,6 +32,8 @@ diag_bef_inr_utr_tid <- function(region = "20", # Enbart ett län i taget.
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_SkapaDiagram.R")
   source("https://raw.githubusercontent.com/Region-Dalarna/hamta_data/refs/heads/main/hamta_bef_region_alder_kon_fodelseregion_tid_InrUtrFoddaRegAlKon_scb.R")
 
+  diagram_capt <- "Källa: Befolkningsregistret i SCB:s öppna statistikdatabas.\nBearbetning: Samhällsanalys, Region Dalarna."
+  gg_list <- list()
 
   bef_bakgr <- hamta_bef_region_alder_kon_fodelseregion_tid_scb(region_vekt = region,
                                                                 alder_koder  = "*",
@@ -37,60 +41,92 @@ diag_bef_inr_utr_tid <- function(region = "20", # Enbart ett län i taget.
                                                                 fodelseregion_klartext = c("Född i Sverige", "Utrikes född"),
                                                                 tid_koder = "*") %>%
     mutate(alder_grupp = skapa_aldersgrupper(ålder,c(20,65))) %>%
-      group_by(år,region,födelseregion,alder_grupp) %>%
-        summarise(Antal = sum(Antal, na.rm = TRUE)) %>%
-          ungroup() %>%
-                mutate(födelseregion = ifelse(födelseregion == "född i Sverige", "Inrikes född", "Utrikes född")) %>%
-            filter(alder_grupp == unique(alder_grupp)[2] )
+    group_by(år,region,födelseregion,alder_grupp) %>%
+    summarise(Antal = sum(Antal, na.rm = TRUE)) %>%
+    ungroup() %>%
+    mutate(födelseregion = ifelse(födelseregion == "född i Sverige", "Inrikes född", "Utrikes född")) %>%
+    filter(alder_grupp == unique(alder_grupp)[2] )
 
-  andel_utrikes_bakgr_df <- bef_bakgr %>%
-    pivot_wider(names_from = "födelseregion", values_from = "Antal") %>%
+  if(diag_andel == TRUE){
+
+    andel_utrikes_bakgr_df <- bef_bakgr %>%
+      pivot_wider(names_from = "födelseregion", values_from = "Antal") %>%
       mutate(`Utrikes födda` = round(`Utrikes född` / (`Inrikes född` + `Utrikes född`) * 100,1),
              `Inrikes födda` = round(`Inrikes född` / (`Inrikes född` + `Utrikes född`) * 100,1)) %>%
-        pivot_longer(cols = c(`Utrikes födda`, `Inrikes födda`),
-                     names_to = "födelseregion",
-                     values_to = "andel")
+      pivot_longer(cols = c(`Utrikes födda`, `Inrikes födda`),
+                   names_to = "födelseregion",
+                   values_to = "andel")
 
 
 
-  if(returnera_data_rmarkdown == TRUE){
-    assign("andel_utrikes_inrikes_bakgr_df", andel_utrikes_bakgr_df, envir = .GlobalEnv)
+    if(returnera_data_rmarkdown == TRUE){
+      assign("andel_utrikes_inrikes_bakgr_df", andel_utrikes_bakgr_df, envir = .GlobalEnv)
+    }
+
+    andel_utrikes_bakgr_df$födelseregion <- factor(andel_utrikes_bakgr_df$födelseregion, levels = c("Utrikes födda","Inrikes födda"))
+
+    diagramtitel <- paste0("Andel i arbetsför ålder (",unique(bef_bakgr$alder_grupp), ") som är inrikes/utrikes födda i ",skapa_kortnamn_lan(unique(bef_bakgr$region)))
+    diagramfilnamn <- paste0("befolkning_utr_inr_andel_",skapa_kortnamn_lan(unique(bef_bakgr$region)),".png")
+
+    gg_obj <- SkapaStapelDiagram(skickad_df = andel_utrikes_bakgr_df,
+                                 skickad_x_var = "år",
+                                 skickad_y_var = "andel",
+                                 skickad_x_grupp = "födelseregion",
+                                 manual_color = diagramfarger("rus_sex"),
+                                 diagram_titel = diagramtitel,
+                                 diagram_capt = diagram_capt,
+                                 x_axis_lutning = 45,
+                                 procent_0_100_10intervaller = TRUE,
+                                 geom_position_stack = TRUE,
+                                 legend_vand_ordning = TRUE,
+                                 diagram_liggande = FALSE,
+                                 manual_x_axis_text_hjust = 1,
+                                 manual_x_axis_text_vjust = 1,
+                                 manual_y_axis_title = "procent",
+                                 stodlinjer_avrunda_fem = TRUE,
+                                 output_mapp = output_mapp,
+                                 filnamn_diagram = diagramfilnamn,
+                                 skriv_till_diagramfil = skriv_diagrambildfil)
+
+    gg_list <- c(gg_list, list(gg_obj))
+    names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+
   }
 
-  gg_list <- list()
+  if(diag_antal == TRUE){
+
+    if(returnera_data_rmarkdown == TRUE){
+      assign("antal_utrikes_inrikes_bakgr_df", bef_bakgr, envir = .GlobalEnv)
+    }
+
+    bef_bakgr$födelseregion <- factor(bef_bakgr$födelseregion, levels = c("Utrikes född","Inrikes född"))
+
+    diagramtitel <- paste0("Befolkning i arbetsför ålder (",unique(bef_bakgr$alder_grupp), ") i ",skapa_kortnamn_lan(unique(bef_bakgr$region)))
+    diagramfilnamn <- paste0("befolkning_utr_inr_antal_",skapa_kortnamn_lan(unique(bef_bakgr$region)),".png")
+
+    gg_obj <- SkapaStapelDiagram(skickad_df = bef_bakgr,
+                                 skickad_x_var = "år",
+                                 skickad_y_var = "Antal",
+                                 skickad_x_grupp = "födelseregion",
+                                 manual_color = diagramfarger("rus_sex"),
+                                 diagram_titel = diagramtitel,
+                                 diagram_capt = diagram_capt,
+                                 x_axis_lutning = 45,
+                                 geom_position_stack = TRUE,
+                                 legend_vand_ordning = TRUE,
+                                 diagram_liggande = FALSE,
+                                 manual_x_axis_text_hjust = 1,
+                                 manual_x_axis_text_vjust = 1,
+                                 stodlinjer_avrunda_fem = TRUE,
+                                 output_mapp = output_mapp,
+                                 filnamn_diagram = diagramfilnamn,
+                                 skriv_till_diagramfil = skriv_diagrambildfil)
 
 
-  diagram_capt <- "Källa: Befolkningsregistret i SCB:s öppna statistikdatabas.\nBearbetning: Samhällsanalys, Region Dalarna."
 
-  andel_utrikes_bakgr_df$födelseregion <- factor(andel_utrikes_bakgr_df$födelseregion, levels = c("Utrikes födda","Inrikes födda"))
-
-  diagramtitel <- paste0("Andel i arbetsför ålder (",unique(bef_bakgr$alder_grupp), ") som är inrikes/utrikes födda i ",skapa_kortnamn_lan(unique(bef_bakgr$region)))
-  diagramfilnamn <- paste0("befolkning_utr_inr_andel_",skapa_kortnamn_lan(unique(bef_bakgr$region)),".png")
-
-  gg_obj <- SkapaStapelDiagram(skickad_df = andel_utrikes_bakgr_df,
-                                    skickad_x_var = "år",
-                                    skickad_y_var = "andel",
-                                    skickad_x_grupp = "födelseregion",
-                                    manual_color = diagramfarger("rus_sex"),
-                                    diagram_titel = diagramtitel,
-                                    diagram_capt = diagram_capt,
-                                    x_axis_lutning = 45,
-                                    procent_0_100_10intervaller = TRUE,
-                                    geom_position_stack = TRUE,
-                                    legend_vand_ordning = TRUE,
-                                    diagram_liggande = FALSE,
-                                    manual_x_axis_text_hjust = 1,
-                                    manual_x_axis_text_vjust = 1,
-                                    manual_y_axis_title = "procent",
-                                    stodlinjer_avrunda_fem = TRUE,
-                                    output_mapp = output_mapp,
-                                    filnamn_diagram = diagramfilnamn,
-                                    skriv_till_diagramfil = skriv_diagrambildfil)
-
-
-
-  gg_list <- c(gg_list, list(gg_obj))
-  names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+    gg_list <- c(gg_list, list(gg_obj))
+    names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+  }
 
   return(gg_list)
 
